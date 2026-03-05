8efd68e7acacff4c0e570339b334150ee5582706
unknown <lauren.smillie.11@gmail.com>
2026-03-05 01:56:24 -0500

    Removing unused files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,43 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import DistanceContext from "./distanceContext";
+import UnitContext from "./unitContext";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { useState } from "react";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const unitState = useState<"m" | "ft">("m");
+  const distanceState = useState<number>(0);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <UnitContext.Provider value={unitState}>
+        <DistanceContext.Provider value={distanceState}>
+          <Stack>
+            <Stack.Screen name="index" options={{ title: "" }} />
+            <Stack.Screen
+              name="measurement-screen"
+              options={{ title: "Measurement" }}
+            />
+            <Stack.Screen
+              name="modal"
+              options={{
+                presentation: "modal",
+                title: "Modal",
+              }}
+            />
+          </Stack>
+          <StatusBar style="auto" />
+        </DistanceContext.Provider>
+      </UnitContext.Provider>
     </ThemeProvider>
   );
 }
